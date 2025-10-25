0769c7a8043350f823f0a38006789cc0578abdf3
Farhan Nawwafal Pramudia <farhan091204@gmail.com>
2025-10-25 17:08:29 +0700

    feat: fetch and show sleep hours from BE

--- src/services/sleepTrackerService.js
+++ src/services/sleepTrackerService.js
@@ -4,5 +4,8 @@ export const getAllSleepTracker = async () => {
     const res = await api.get('/sleep/user');
     return res.data.data;
 };
-
-export const addSleepTracker = async data => api.post('/sleep', data);
+export const addSleepTracker = async data => await api.post('/sleep', data);
+export const getAverageSleep = async () => {
+    const res = await api.get('/sleep/average');
+    return res.data.data;
+};
