81387cdc0d87fbf5eab4129f7a93bdae1dd62cfe
Farhan Nawwafal Pramudia <farhan091204@gmail.com>
2025-11-14 23:52:17 +0700

    refactor: baseURL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https:api.heylth.space/api',
+    baseURL: 'https://api.heylth.space/api',
 });
 
 api.interceptors.request.use(config => {
