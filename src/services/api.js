f94514b55562dfc22533ef2bd4b996ae5ad90acd
Farhan Nawwafal Pramudia <farhan091204@gmail.com>
2025-12-23 10:22:25 +0700

    fix: update baseURL FE

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    // baseURL: 'https://api.heylth.space/api',
-    baseURL: 'http://localhost:3000/api',
+    baseURL: 'https://api.heylth.space/api',
 });
 
 api.interceptors.request.use(config => {
