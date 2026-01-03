ae6c6a393e7a6969de25aefdec64cadaa2393d1d
Farhan-Nawwafal <farhan091204@gmail.com>
2026-01-03 12:21:20 +0700

    chore: update baseURL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://api.heylth.space/api',
+    // baseURL: 'https://api.heylth.space/api',
+    baseURL: 'http://localhost:3000/api',
 });
 
 api.interceptors.request.use(config => {
